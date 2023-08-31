085137edb7c2da8b15644b952cc9c7f533af29dc
Taz17 <mutaz.ashhab@gmail.com>
2023-08-31 20:44:06 +1000

    use new `FileUploadButton`

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,15 @@
 'use client';
 
+import { FileUploadButton } from '@/components/Buttons';
+import { Button } from '@/components/Buttons/Button/Button';
 import { DataTable } from '@/components/Table/DataTable';
-import { FileUploadLogic } from '@/logic/FileUploadLogic';
-import { useTableStore } from '@/zustand/useTableStore';
 
 export default function Home() {
-  const tableStore = useTableStore();
-
-  async function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
-    const file = e.target.files?.[0];
-    if (!file) return;
-
-    const data = await FileUploadLogic.parseData(file);
-
-    if (data) {
-      tableStore.setTable(data);
-    }
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>Upload CSV/Excel File</h1>
-        <input
-          type="file"
-          accept=".csv, .xlsx, .xls"
-          onChange={handleFileUpload}
-        />
+        <FileUploadButton />
         <DataTable />
       </div>
     </main>
