8d3b3ca56c8021d510cd86dfbedf33b126d9ef24
Taz17 <mutaz.ashhab@gmail.com>
2023-08-31 21:20:54 +1000

    style table

--- components/Table/DataTable.tsx
+++ components/Table/DataTable.tsx
@@ -9,19 +9,32 @@ export function DataTable() {
   const rows = tableStore.data.slice(1);
 
   return (
-    <table>
-      <thead>
+    <table className="min-w-full bg-white border border-gray-300">
+      <thead className="bg-gray-100">
         <tr>
           {headers.map((header, index) => (
-            <th key={index}>{header}</th>
+            <th
+              key={index}
+              className="py-2 px-4 border-b border-gray-300 text-left text-sm leading-4 text-gray-500 uppercase tracking-wider"
+            >
+              {header}
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
         {rows.map((row, index) => (
-          <tr key={index}>
+          <tr
+            key={index}
+            className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}
+          >
             {row.map((cell, i) => (
-              <td key={i}>{cell}</td>
+              <td
+                key={i}
+                className="py-2 px-4 border-b border-gray-300 text-gray-800"
+              >
+                {cell}
+              </td>
             ))}
           </tr>
         ))}
