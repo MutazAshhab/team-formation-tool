bcff0299ec771e8a22056119279054bfc1cbe6dc
Taz17 <mutaz.ashhab@gmail.com>
2023-09-02 22:48:45 +1000

    Custom footer for explainer view

--- components/TeamFormationDialog/Views/Explainer.tsx
+++ components/TeamFormationDialog/Views/Explainer.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/Buttons';
+import { DialogFooter } from '@/components/Dialog/DialogFooter';
 import { DialogHeader } from '@/components/Dialog/DialogHeader';
 import { useTeamFormationStepsStore } from '@/zustand/useTeamFormationStepsStore';
 
@@ -6,13 +8,23 @@ import { TeamFormationStepsDialogFooter } from '../TeamFormationStepsDialogFoote
 export function Explainer() {
   const teamFormationStore = useTeamFormationStepsStore();
 
+  function handleDefaultAlgorithmClick() {}
+  function handleCustomAlgorithmClick() {}
+
   return (
     <>
       <DialogHeader
         closeModal={teamFormationStore.closeTeamFormationModal}
         title="Explainer"
       />
-      <TeamFormationStepsDialogFooter onNextClick={() => {}} />
+      <DialogFooter>
+        <div className="flex justify-around items-center">
+          <Button onClick={handleDefaultAlgorithmClick}>
+            Default Algorithm
+          </Button>
+          <Button onClick={handleCustomAlgorithmClick}>Custom Algorithm</Button>
+        </div>
+      </DialogFooter>
     </>
   );
 }
