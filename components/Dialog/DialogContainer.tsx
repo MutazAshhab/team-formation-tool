a336553c282b7db2accefd07c0e4b827b5bce51e
Taz17 <mutaz.ashhab@gmail.com>
2023-09-02 20:57:30 +1000

    add `DialogContainer`

---
+++ components/Dialog/DialogContainer.tsx
@@ -0,0 +1,22 @@
+import { createPortal } from 'react-dom';
+
+interface DialogContainerProps {
+  show: boolean;
+  closeModal: () => void;
+  children: React.ReactNode;
+}
+
+export function DialogContainer(props: DialogContainerProps) {
+  if (!props.show) {
+    return null;
+  }
+
+  return createPortal(
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <dialog className="flex items-center justify-center w-3/4 rounded bg-white p-4">
+        {props.children}
+      </dialog>
+    </div>,
+    document.body,
+  );
+}
