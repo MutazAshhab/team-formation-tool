96d01bb888baed7fb359657526179d46d135ddec
Taz17 <mutaz.ashhab@gmail.com>
2023-09-02 19:16:44 +1000

    add views instead of steps

--- zustand/useTeamFormationStepsStore.ts
+++ zustand/useTeamFormationStepsStore.ts
@@ -1,9 +1,27 @@
 import { create } from 'zustand';
 
+const views = {
+  explainer: 'explainer', // First page
+
+  // Default algorithm
+  defaultAlgorithmExplainer: 'defaultAlgorithmExplainer', // Default algorithm explainer
+  defaultAlgorithmMapping: 'defaultAlgorithmMapping', // Where the user maps their data to the default algorithm parameters
+
+  customAlgorithmExplainer: 'customAlgorithmExplainer', // Custom algorithm explainer
+  customAlgorithmMapping: 'customAlgorithmMapping', // Where the user maps their data to the constraints that they want
+
+  complete: 'complete', // All steps complete
+
+  developerError: 'developerError', // Shown the flow is configured incorrectly
+} as const;
+
+// type ViewNames = keyof typeof views;
+type ViewNames = (typeof views)[keyof typeof views];
+
 type TeamFormationStepsStore = {
-  step: number;
-  gotoNextStep: () => void;
-  gotoStep: (i: number) => void;
+  view: ViewNames;
+  gotoNextView: () => void;
+  gotoView: (viewName: ViewNames) => void;
   showTeamFormationModal: boolean;
   closeTeamFormationModal: () => void;
   openTeamFormationModal: () => void;
@@ -11,20 +29,42 @@ type TeamFormationStepsStore = {
 
 export const useTeamFormationStepsStore = create<TeamFormationStepsStore>(
   set => ({
-    step: 0,
-    gotoNextStep: () => {
+    view: views.explainer,
+    gotoNextView: () => {
       set(state => {
-        return {
-          step: state.step + 1,
-        };
+        switch (state.view) {
+          case views.explainer:
+            return {
+              // We cannot decide where the user will go after the explainer, use the gotoView, along side the option the user requests.
+              view: views.developerError,
+            };
+          case views.defaultAlgorithmExplainer:
+            return {
+              view: views.defaultAlgorithmMapping,
+            };
+          case views.defaultAlgorithmMapping:
+            return {
+              view: views.complete,
+            };
+          case views.customAlgorithmExplainer:
+            return {
+              view: views.customAlgorithmMapping,
+            };
+          case views.customAlgorithmMapping:
+            return {
+              view: views.complete,
+            };
+        }
+
+        return { view: views.developerError };
       });
     },
-    gotoStep: (i: number) => {
-      set({ step: i });
+    gotoView: (viewName: ViewNames) => {
+      set({ view: viewName });
     },
     showTeamFormationModal: false,
     closeTeamFormationModal: () => {
-      set({ showTeamFormationModal: false });
+      set({ showTeamFormationModal: false, view: views.explainer });
     },
     openTeamFormationModal: () => {
       set({ showTeamFormationModal: true });
