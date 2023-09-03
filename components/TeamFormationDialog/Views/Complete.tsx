663b918d04504f1f72ec4e073a04ed328ea4f52e
Taz17 <mutaz.ashhab@gmail.com>
2023-09-03 22:06:28 +1000

    log the algorithm store when the user clicks next on the complete view

--- components/TeamFormationDialog/Views/Complete.tsx
+++ components/TeamFormationDialog/Views/Complete.tsx
@@ -1,10 +1,12 @@
 import { DialogHeader } from '@/components/Dialog/DialogHeader';
+import { useAlgorithmStore } from '@/zustand/useAlgorithmStore';
 import { useTeamFormationStepsStore } from '@/zustand/useTeamFormationStepsStore';
 
 import { TeamFormationStepsDialogFooter } from '../TeamFormationStepsDialogFooter';
 
 export function Complete() {
   const teamFormationStore = useTeamFormationStepsStore();
+  const algorithmStore = useAlgorithmStore();
 
   return (
     <>
@@ -12,7 +14,11 @@ export function Complete() {
         closeModal={teamFormationStore.closeTeamFormationModal}
         title="Complete"
       />
-      <TeamFormationStepsDialogFooter onNextClick={() => {}} />
+      <TeamFormationStepsDialogFooter
+        onNextClick={() => {
+          console.log(algorithmStore);
+        }}
+      />
     </>
   );
 }
