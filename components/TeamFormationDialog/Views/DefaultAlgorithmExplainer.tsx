9e54e07307a21459f2a3db277a7d27dabbd08b4b
Taz17 <mutaz.ashhab@gmail.com>
2023-09-04 20:40:21 +1000

    add default algorithm explainer copy

--- components/TeamFormationDialog/Views/DefaultAlgorithmExplainer.tsx
+++ components/TeamFormationDialog/Views/DefaultAlgorithmExplainer.tsx
@@ -1,3 +1,5 @@
+import { InfoAlertBox } from '@/components/AlertBoxes/InfoAlertBox';
+import { DialogContent } from '@/components/Dialog/DialogContent';
 import { DialogHeader } from '@/components/Dialog/DialogHeader';
 import { useTeamFormationStepsStore } from '@/zustand/useTeamFormationStepsStore';
 
@@ -12,6 +14,19 @@ export function DefaultAlgorithmExplainer() {
         closeModal={teamFormationStore.closeTeamFormationModal}
         title="DefaultAlgorithmExplainer"
       />
+      <DialogContent>
+        <InfoAlertBox>
+          Our default algorithm aims to create balanced and diverse teams for
+          you. It ensures that each team has a mix of genders, language
+          backgrounds, and academic performance levels. The algorithm also takes
+          into account individual traits like agreeability and anxiety levels to
+          foster a harmonious team environment.
+          <br />
+          <br />
+          In the next step, you will be asked to match the data columns to the
+          factors, and let the algorithm do the work!
+        </InfoAlertBox>
+      </DialogContent>
       <TeamFormationStepsDialogFooter
         onNextClick={teamFormationStore.gotoNextView}
       />
