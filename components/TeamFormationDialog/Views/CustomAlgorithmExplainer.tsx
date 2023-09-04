a12e452bd8dd97a9809408fac1d95282a40c237a
Taz17 <mutaz.ashhab@gmail.com>
2023-09-04 21:12:05 +1000

    add custom algo explainer message

--- components/TeamFormationDialog/Views/CustomAlgorithmExplainer.tsx
+++ components/TeamFormationDialog/Views/CustomAlgorithmExplainer.tsx
@@ -1,3 +1,5 @@
+import { InfoAlertBox } from '@/components/AlertBoxes/InfoAlertBox';
+import { DialogContent } from '@/components/Dialog/DialogContent';
 import { DialogHeader } from '@/components/Dialog/DialogHeader';
 import { useTeamFormationStepsStore } from '@/zustand/useTeamFormationStepsStore';
 
@@ -12,6 +14,20 @@ export function CustomAlgorithmExplainer() {
         closeModal={teamFormationStore.closeTeamFormationModal}
         title="CustomAlgorithmExplainer"
       />
+      <DialogContent>
+        <InfoAlertBox>
+          Want more control over your teams? Our custom algorithm lets you
+          tailor the team formation to your specific needs. You can select which
+          data columns to use and apply your own constraints. Whether you want
+          at least one team member to have taken a specific elective or
+          you&apos;re looking to limit the number of extroverted individuals in
+          a team, you have the freedom to set it all up.
+          <br />
+          <br />
+          Just match the data columns to your desired factors, add any extra
+          rules, and let the algorithm create the perfect teams for you!
+        </InfoAlertBox>
+      </DialogContent>
       <TeamFormationStepsDialogFooter
         onNextClick={teamFormationStore.gotoNextView}
       />
