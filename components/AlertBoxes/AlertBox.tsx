b0ef4c098372773b285fa53e9b4da43e91105d6c
Taz17 <mutaz.ashhab@gmail.com>
2023-09-04 21:59:25 +1000

    use `variant` instead of having different files

--- components/AlertBoxes/AlertBox.tsx
+++ components/AlertBoxes/AlertBox.tsx
@@ -1,14 +1,31 @@
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+
 interface AlertBoxProps {
-  icon: JSX.Element;
-  color: string;
+  variant: 'info' | 'error';
   children: React.ReactNode;
 }
 
 export function AlertBox(props: AlertBoxProps) {
+  const colors = (() => {
+    switch (props.variant) {
+      case 'info':
+        return {
+          background: 'bg-yellow-100',
+          icon: 'text-yellow-700',
+        };
+      case 'error':
+        return {
+          background: 'bg-red-100',
+          icon: 'text-red-700',
+        };
+    }
+  })();
   return (
-    <div className={`p-4 rounded ${props.color}`}>
+    <div className={`p-4 rounded ${colors.background}`}>
       <div className="flex items-center">
-        <div className="mr-4">{props.icon}</div>
+        <div className="mr-4">
+          <ExclamationTriangleIcon className={`h-6 w-6 ${colors.icon}`} />
+        </div>
         <div>{props.children}</div>
       </div>
     </div>
