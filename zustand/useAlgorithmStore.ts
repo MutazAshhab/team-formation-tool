6eb663e031bc179ad15a066cb055a9358d136d26
Taz17 <mutaz.ashhab@gmail.com>
2023-09-05 11:23:57 +1000

    add `teamSize` and `setTeamSize` to `algorithmStore`

--- zustand/useAlgorithmStore.ts
+++ zustand/useAlgorithmStore.ts
@@ -5,6 +5,8 @@ type AlgorithmOptions = 'default' | 'custom';
 type AlgorithmStore = {
   chosenAlgorithm: AlgorithmOptions | null;
   setChosenAlgorithm: (option: AlgorithmOptions) => void;
+  teamSize: number | null;
+  setTeamSize: (size: number) => void;
   reset: () => void;
 };
 
@@ -12,6 +14,10 @@ export const useAlgorithmStore = create<AlgorithmStore>(set => ({
   chosenAlgorithm: null,
   setChosenAlgorithm: option => {
     set({ chosenAlgorithm: option });
+  },
+  teamSize: null,
+  setTeamSize: (size: number) => {
+    set({ teamSize: size });
   },
   reset: () => {
     set({ chosenAlgorithm: null });
