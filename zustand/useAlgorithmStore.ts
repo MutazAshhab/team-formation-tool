e908448cc05c5061304305ccfbe334a966d4aa92
Taz17 <mutaz.ashhab@gmail.com>
2023-09-05 11:44:55 +1000

    add mapping to algorithm store

--- zustand/useAlgorithmStore.ts
+++ zustand/useAlgorithmStore.ts
@@ -2,12 +2,26 @@ import { create } from 'zustand';
 
 type AlgorithmOptions = 'default' | 'custom';
 
+interface Mapping {
+  columnName: string;
+}
+
+interface DefaultMapping extends Mapping {
+  optionName: string;
+}
+
+interface CustomMapping extends Mapping {
+  constraint: string; // String for now
+}
+
 type AlgorithmStore = {
   chosenAlgorithm: AlgorithmOptions | null;
   setChosenAlgorithm: (option: AlgorithmOptions) => void;
   teamSize: number | null;
   setTeamSize: (size: number) => void;
   reset: () => void;
+  mapping: (DefaultMapping | CustomMapping)[];
+  setMapping: (mapping: (DefaultMapping | CustomMapping)[]) => void;
 };
 
 export const useAlgorithmStore = create<AlgorithmStore>(set => ({
@@ -20,6 +34,10 @@ export const useAlgorithmStore = create<AlgorithmStore>(set => ({
     set({ teamSize: size });
   },
   reset: () => {
-    set({ chosenAlgorithm: null });
+    set({ chosenAlgorithm: null, teamSize: null });
+  },
+  mapping: [],
+  setMapping: (mapping: (DefaultMapping | CustomMapping)[]) => {
+    set({ mapping });
   },
 }));
