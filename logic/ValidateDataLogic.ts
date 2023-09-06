534e11a4ba618968b49b54857178f3bceb1cb09e
Taz17 <mutaz.ashhab@gmail.com>
2023-09-06 15:38:28 +1000

    format code

--- logic/ValidateDataLogic.ts
+++ logic/ValidateDataLogic.ts
@@ -1,4 +1,4 @@
-import Ajv from "ajv";
+import Ajv from 'ajv';
 import addFormats from 'ajv-formats';
 
 export const ValidateDataLogic = { validateData };
@@ -7,28 +7,34 @@ const ajvInstance = new Ajv({ allErrors: true });
 addFormats(ajvInstance);
 
 const schema = {
-    type: 'object',
-    properties: {
-      mapping: {
-        type: 'array',
-        items: {
-          type: 'object',
-          properties: {
-            columnName: { type: 'string' },
-            optionName: {
-              type: 'string',
-              enum: ['gender', 'first_language', 'wam', 'anxiety', 'agreeableness'],
-            },
+  type: 'object',
+  properties: {
+    mapping: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          columnName: { type: 'string' },
+          optionName: {
+            type: 'string',
+            enum: [
+              'gender',
+              'first_language',
+              'wam',
+              'anxiety',
+              'agreeableness',
+            ],
           },
-          required: ['columnName', 'optionName'],
         },
+        required: ['columnName', 'optionName'],
       },
     },
-    required: ['mapping'],
+  },
+  required: ['mapping'],
 };
 
 function validateData(data: any): boolean {
-    const validate = ajvInstance.compile(schema);
-    const isValid = validate(data);
-    return isValid;
-};
+  const validate = ajvInstance.compile(schema);
+  const isValid = validate(data);
+  return isValid;
+}
