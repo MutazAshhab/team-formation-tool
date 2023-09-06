ca340d2703079ab6c4c5f2bc7d123fdcbb7c21bb
Taz17 <mutaz.ashhab@gmail.com>
2023-09-06 17:18:03 +1000

    Update button styling

--- components/Buttons/Button/Button.tsx
+++ components/Buttons/Button/Button.tsx
@@ -15,10 +15,10 @@ export function Button({
 }: ButtonProps) {
   return (
     <button
-      className="inline-flex align-self-start max-w-fit"
+      className="inline-flex align-self-start max-w-fit cursor-pointer bg-blue-500 text-white px-4 py-2 rounded duration-300 ease-in-out hover:bg-blue-700"
       onClick={onClick}
     >
-      <div className="flex flex-row gap-2 border border-slate-400 text-black py-3 px-4 text-lg transition duration-300 ease-in-out hover:bg-gray-100 hover:border-slate-600 rounded-lg">
+      <div className="flex flex-row gap-2">
         {iconPosition === 'left' && icon}
         {children}
         {iconPosition === 'right' && icon}
