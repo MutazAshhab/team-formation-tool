18ecdb846d35a73babda4acfff8ffc2bee665644
Chokss54 <joelyang54@gmail.com>
2023-09-06 15:26:26 +1000

    1. Implemented ValidateDataLogic that validates the schema of JSON data input 2. Implemented TeamFormationLogic that handles the backend logic of our tool (incomplete)

---
+++ logic/ValidateDataLogic.ts
@@ -0,0 +1,34 @@
+import Ajv from "ajv";
+import addFormats from 'ajv-formats';
+
+export const ValidateDataLogic = { validateData };
+
+const ajvInstance = new Ajv({ allErrors: true });
+addFormats(ajvInstance);
+
+const schema = {
+    type: 'object',
+    properties: {
+      mapping: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            columnName: { type: 'string' },
+            optionName: {
+              type: 'string',
+              enum: ['gender', 'first_language', 'wam', 'anxiety', 'agreeableness'],
+            },
+          },
+          required: ['columnName', 'optionName'],
+        },
+      },
+    },
+    required: ['mapping'],
+};
+
+function validateData(data: any): boolean {
+    const validate = ajvInstance.compile(schema);
+    const isValid = validate(data);
+    return isValid;
+};
