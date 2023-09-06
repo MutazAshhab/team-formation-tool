249504eadbeeb1ffe8d4d9c1d7986cd0fe811d04
Taz17 <mutaz.ashhab@gmail.com>
2023-09-06 17:23:19 +1000

    better props for button

--- components/Buttons/Button/Button.tsx
+++ components/Buttons/Button/Button.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { AllHTMLAttributes } from 'react';
 
-interface ButtonProps {
-  onClick?: () => void;
+interface ButtonProps extends NativeButtonProps {
   children?: React.ReactNode;
   icon?: React.ReactNode;
   iconPosition?: 'left' | 'right';
 }
+
+type NativeButtonProps = AllHTMLAttributes<HTMLButtonElement>;
 
 export function Button({
   onClick,
   children,
   icon = undefined,
   iconPosition,
+  className,
+  ...rest
 }: ButtonProps) {
+  const classNames = [
+    'inline-flex align-self-start max-w-fit cursor-pointer bg-blue-500 text-white px-4 py-2 rounded duration-300 ease-in-out hover:bg-blue-700',
+    className ?? '',
+  ].join(' ');
+
   return (
-    <button
-      className="inline-flex align-self-start max-w-fit cursor-pointer bg-blue-500 text-white px-4 py-2 rounded duration-300 ease-in-out hover:bg-blue-700"
-      onClick={onClick}
-    >
+    <button className={classNames} onClick={onClick}>
       <div className="flex flex-row gap-2">
         {iconPosition === 'left' && icon}
         {children}
