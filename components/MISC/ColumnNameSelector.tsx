d032cc178d4aba60204a35d6c3c3f040402c2b33
Taz17 <mutaz.ashhab@gmail.com>
2023-09-09 21:25:04 +1000

    add `ColumnNameSelector`

---
+++ components/MISC/ColumnNameSelector.tsx
@@ -0,0 +1,47 @@
+import { useTableStore } from '@/zustand/useTableStore';
+
+import { AlertBox } from '../AlertBoxes/AlertBox';
+
+interface ColumnNameSelectorProps {
+  label: string;
+  onSelect: (value: string) => void;
+}
+
+export function ColumnNameSelector(props: ColumnNameSelectorProps) {
+  const table = useTableStore();
+
+  if (table.data.length === 0) {
+    return (
+      <AlertBox variant="error">
+        Some how you are here whilst having 0 data
+      </AlertBox>
+    );
+  }
+
+  function handleSelectChange(value: string) {
+    props.onSelect(value);
+  }
+
+  const headers = table.data[0];
+
+  return (
+    <div className="p-2 border border-gray-200 rounded-lg w-[96%]">
+      <label className="block text-md font-medium text-gray-700">
+        {props.label}
+      </label>
+      <select
+        onChange={e => handleSelectChange(e.target.value)}
+        className="mt-2 w-full p-3 border border-gray-300 rounded-lg"
+      >
+        <option value="" className="text-gray-400">
+          Select the respective header
+        </option>
+        {headers.map(header => (
+          <option key={header} value={header} className="text-gray-700">
+            {header}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
