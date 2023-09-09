58e024143e3160db472a7b11449026c3ec924fdc
Taz17 <mutaz.ashhab@gmail.com>
2023-09-09 23:08:31 +1000

    update `ColumnValuesErrorBox`

--- components/TeamFormationDialog/Views/ColumnValuesErrorBox.tsx
+++ components/TeamFormationDialog/Views/ColumnValuesErrorBox.tsx
@@ -2,7 +2,11 @@ import { AlertBox } from '@/components/AlertBoxes/AlertBox';
 
 import { ColumnValuesErrorType } from './types';
 
-export function ColumnValuesErrorBox(props: { type: ColumnValuesErrorType }) {
+interface ColumnValuesErrorBoxProps {
+  type: ColumnValuesErrorType;
+}
+
+export function ColumnValuesErrorBox(props: ColumnValuesErrorBoxProps) {
   if (props.type === null) {
     return null;
   }
@@ -12,15 +16,39 @@ export function ColumnValuesErrorBox(props: { type: ColumnValuesErrorType }) {
       Please select a <strong>column name</strong>
     </p>
   );
+
   const valuesErrorMessage = (
     <p>
       Please select <strong>at least one value</strong>
     </p>
   );
 
-  return (
-    <AlertBox variant="error">
-      {props.type === 'name' ? nameErrorMessage : valuesErrorMessage}
-    </AlertBox>
+  const minErrorMessage = (
+    <p>
+      Please set a <strong>minimum value</strong>
+    </p>
   );
+
+  const maxErrorMessage = (
+    <p>
+      Please set a <strong>maximum value</strong>
+    </p>
+  );
+
+  const messageToDisplay = (() => {
+    switch (props.type) {
+      case 'name':
+        return nameErrorMessage;
+      case 'value':
+        return valuesErrorMessage;
+      case 'min':
+        return minErrorMessage;
+      case 'max':
+        return maxErrorMessage;
+      default:
+        return null;
+    }
+  })();
+
+  return <AlertBox variant="error">{messageToDisplay}</AlertBox>;
 }
