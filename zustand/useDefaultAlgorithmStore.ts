0ac3a2e18d036a8fca4152a63e603d2448d32b7a
Taz17 <mutaz.ashhab@gmail.com>
2023-09-09 19:47:50 +1000

    update `DefaultAlgorithmStore`

--- zustand/useDefaultAlgorithmStore.ts
+++ zustand/useDefaultAlgorithmStore.ts
@@ -1,25 +1,57 @@
 import { create } from 'zustand';
 
-export interface DefaultMapping {
-  gender: string;
-  first_language: string;
-  wam: string;
-  anxiety: string;
-  agreeableness: string;
-}
-
 type DefaultAlgorithmStore = {
-  mapping: DefaultMapping | null;
-  setMapping: (mapping: DefaultMapping) => void;
+  gender: string | null;
+  setGender: (gender: string) => void;
+  first_language: string[] | null;
+  setFirstLanguage: (first_language: string[]) => void;
+  wam: string | null;
+  setWam: (wam: string) => void;
+  anxiety: string | null;
+  setAnxiety: (anxiety: string) => void;
+  agreeableness: string | null;
+  setAgreeableness: (agreeableness: string) => void;
   reset: () => void;
 };
 
 export const useDefaultAlgorithmStore = create<DefaultAlgorithmStore>(set => ({
-  mapping: null,
-  setMapping: (mapping: DefaultMapping) => {
-    set({ mapping });
+  gender: null,
+  setGender: (gender: string) => {
+    set({
+      gender,
+    });
+  },
+  first_language: null,
+  setFirstLanguage: (first_language: string[]) => {
+    set({
+      first_language,
+    });
+  },
+  wam: null,
+  setWam: (wam: string) => {
+    set({
+      wam,
+    });
+  },
+  anxiety: null,
+  setAnxiety: (anxiety: string) => {
+    set({
+      anxiety,
+    });
+  },
+  agreeableness: null,
+  setAgreeableness: (agreeableness: string) => {
+    set({
+      agreeableness,
+    });
   },
   reset: () => {
-    set({ mapping: null });
+    set({
+      gender: null,
+      first_language: null,
+      wam: null,
+      anxiety: null,
+      agreeableness: null,
+    });
   },
 }));
