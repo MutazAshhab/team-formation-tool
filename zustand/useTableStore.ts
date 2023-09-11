34ef3625ccbfa1d12d0df5ba2f8458ba930661d2
Taz17 <mutaz.ashhab@gmail.com>
2023-09-11 14:00:02 +1000

    add `formedTeams` and `setFormedTeams` to `tableStore`

--- zustand/useTableStore.ts
+++ zustand/useTableStore.ts
@@ -3,11 +3,17 @@ import { create } from 'zustand';
 type TableStore = {
   data: string[][];
   setTable: (data: string[][]) => void;
+  formedTeams: string[][];
+  setFormedTeams: (data: string[][]) => void;
 };
 
 export const useTableStore = create<TableStore>(set => ({
   data: [],
   setTable: (data: string[][]) => {
     set({ data });
+  },
+  formedTeams: [],
+  setFormedTeams: (formedTeams: string[][]) => {
+    set({ formedTeams });
   },
 }));
