db4e19c56c99a8135fe3073537c83853cd6e1faf
Taz17 <mutaz.ashhab@gmail.com>
2023-10-18 19:57:50 +1100

    update `ColumnConstraintSection`

--- components/TeamFormationDialog/Views/CustomAlgorithm/Constraints/ColumnConstraintSection.tsx
+++ components/TeamFormationDialog/Views/CustomAlgorithm/Constraints/ColumnConstraintSection.tsx
@@ -2,38 +2,49 @@ import React, { useState } from 'react';
 
 import { ColumnNameSelector } from '@/components/MISC/ColumnNameSelector';
 
-import { Constraint, ConstraintSelector } from './ConstraintSelector';
-import { ConstraintSummary } from './ConstraintSummary';
+import { ConstraintSelector } from './ConstraintSelector';
 
-export function ColumnConstraintSection() {
-  const [constraints, setConstraints] = useState<Constraint[]>([]);
-  const [currentConstraint, setCurrentConstraint] = useState<Constraint>({
-    column: '',
-    type: '',
-    value: '',
-  });
+interface Constraint {
+  column: string | null;
+  type: string;
+  value: string | number;
+}
 
-  const handleAddConstraint = () => {
-    setConstraints([...constraints, currentConstraint]);
-    setCurrentConstraint({ column: '', type: '', value: '' });
-  };
+interface ColumnConstraintSectionProps {
+  constraint: Constraint;
+  onConstraintChange: (index: number, updatedConstraint: Constraint) => void;
+  removeConstraint: (index: number) => void;
+  index: number;
+}
+
+export function ColumnConstraintSection(props: ColumnConstraintSectionProps) {
+  function updateConstraint(updatedConstraint: Constraint) {
+    props.onConstraintChange(props.index, updatedConstraint);
+  }
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 border border-gray-200 rounded-lg pt-2 pl-2 pb-2">
+      <button
+        className="text-red-500"
+        onClick={() => props.removeConstraint(props.index)}
+      >
+        Remove
+      </button>
       <ColumnNameSelector
         label="Select the column to apply the constraint on"
         onSelect={value =>
-          setCurrentConstraint({
-            ...currentConstraint,
+          updateConstraint({
+            ...props.constraint,
             column: value,
           })
         }
-        selectedValue={currentConstraint.column}
+        selectedValue={props.constraint.column}
       />
 
-      {/* <ConstraintSelector ></ConstraintSelector> */}
-
-      <ConstraintSummary constraints={constraints}></ConstraintSummary>
+      <ConstraintSelector
+        onConstraintChange={() => {}}
+        selectedType=""
+      ></ConstraintSelector>
     </div>
   );
 }
