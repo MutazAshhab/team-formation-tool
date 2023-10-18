c07a3aca77a703182a31e677862f011b2c5a516c
Taz17 <mutaz.ashhab@gmail.com>
2023-10-18 19:49:03 +1100

    Update `ConstraintSummary`

--- components/TeamFormationDialog/Views/CustomAlgorithm/Constraints/ConstraintSummary.tsx
+++ components/TeamFormationDialog/Views/CustomAlgorithm/Constraints/ConstraintSummary.tsx
@@ -5,28 +5,27 @@ interface ConstraintSummaryProps {
 }
 
 export function ConstraintSummary(props: ConstraintSummaryProps) {
-  const summaryListItems = props.constraints.map((constraint, index) => (
-    <li key={index}>
-      {constraint.type === 'minUnique'
-        ? `At least ${constraint.value} unique values from '${constraint.column}' column.`
-        : constraint.type === 'mustContain'
-        ? `'${constraint.column}' column must contain '${constraint.value}'.`
-        : ''}
-    </li>
-  ));
+  const shouldShowSummary = props.constraints.length > 0;
 
-  const emptyMessage = 'No constraints applied yet.';
+  // TODO: Actially implement this
+  if (!shouldShowSummary || true) {
+    return null;
+  }
 
   return (
     <div className="flex flex-col gap-4 p-2 border border-gray-200 rounded-lg w-[96%]">
       <p className="block text-md font-medium text-gray-700">
         Currently Applied Constraints
       </p>
-      {summaryListItems.length === 0 ? (
-        emptyMessage
-      ) : (
-        <ul>{summaryListItems}</ul>
-      )}
+      {props.constraints.map((constraint, index) => (
+        <li key={index}>
+          {constraint.type === 'minUnique'
+            ? `At least ${constraint.value} unique values from '${constraint.column}' column.`
+            : constraint.type === 'mustContain'
+            ? `'${constraint.column}' column must contain '${constraint.value}'.`
+            : ''}
+        </li>
+      ))}
     </div>
   );
 }
