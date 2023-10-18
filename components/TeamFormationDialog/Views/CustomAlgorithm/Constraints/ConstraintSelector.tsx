29738d13a96852488c35b053baf9e962de23ff0d
Taz17 <mutaz.ashhab@gmail.com>
2023-10-18 19:58:05 +1100

    update `ConstraintSelector`

--- components/TeamFormationDialog/Views/CustomAlgorithm/Constraints/ConstraintSelector.tsx
+++ components/TeamFormationDialog/Views/CustomAlgorithm/Constraints/ConstraintSelector.tsx
@@ -18,24 +18,31 @@ export function ConstraintSelector({
   selectedValue,
 }: ConstraintSelectorProps) {
   return (
-    <div className="flex gap-4 mt-4">
-      <select
-        value={selectedType || ''}
-        onChange={e => onConstraintChange(e.target.value, selectedValue || '')}
-        className="border p-2 rounded"
-      >
-        <option value="">Choose a constraint</option>
-        <option value="minUnique">Minimum number of unique values</option>
-        <option value="mustContain">Must contain value</option>
-      </select>
+    <div className="flex flex-col gap-4 p-2 border border-gray-200 rounded-lg w-[96%]">
+      <label className="block text-md font-medium text-gray-700">
+        Select a constraint for this heading
+      </label>
+      <div className="flex flex-row justify-between">
+        <select
+          value={selectedType || ''}
+          onChange={e =>
+            onConstraintChange(e.target.value, selectedValue || '')
+          }
+          className="border p-2 rounded"
+        >
+          <option value="">Select a constraint</option>
+          <option value="minUnique">Minimum number of unique values</option>
+          <option value="mustContain">Must contain value</option>
+        </select>
 
-      <input
-        type="text"
-        value={String(selectedValue || '')}
-        onChange={e => onConstraintChange(selectedType || '', e.target.value)}
-        className="border p-2 rounded"
-        placeholder="Enter value"
-      />
+        <input
+          type="text"
+          value={String(selectedValue || '')}
+          onChange={e => onConstraintChange(selectedType || '', e.target.value)}
+          className="border p-2 rounded"
+          placeholder="Enter value"
+        />
+      </div>
     </div>
   );
 }
